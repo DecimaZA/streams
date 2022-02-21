5b281cd7f204f2f099992a3a047934d76c587d72
Jonathan Wright <devorakwright@gmail.com>
2022-02-21 15:27:05 +0200

    added redux-form code

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from 'redux-form'; //this syntax for renaming 'reducer' is just an example of a more descriptive way to reference this variable
 import authReducer from './authReducer';
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
