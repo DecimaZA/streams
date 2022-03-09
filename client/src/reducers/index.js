c8422d9d415ddc66d54e12dbf499e5a321aaa80f
Jonathan Wright <devorakwright@gmail.com>
2022-03-09 11:23:22 +0200

    another commit

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from 'redux-form'; //this syntax for renaming 'reducer' is just an example of a more descriptive way to reference this variable
 import authReducer from './authReducer';
+import streamReducer from "./streamReducer";
 
 export default combineReducers({
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    streams: streamReducer
 });
